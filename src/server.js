4700d699ea434221070f2033bfbf6dbb88d8dc2e
eunsoooo <shines1427@gmail.com>
2022-06-06 02:58:46 +0900

    feat: 컨트롤러, 미들웨어 테스트를 위한 간단한 코드 작성

--- src/server.js
+++ src/server.js
@@ -1,7 +1,27 @@
 import express from "express";
+import res from "express/lib/response";
 
 const PORT = 3000;  // TODO: env 파일로 빼기
+
 const app = express();
+
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+const handleHome = (req, res) => {
+  return res.send("home");
+};
+
+const handleLogin = (req, res) => {
+  return res.send("login");
+};
+
+app.get("/", logger, handleHome);
+
+app.get("/login", logger, handleLogin);
+
 const handleListening = () => {
   console.log(`✅ Server listening on port ${PORT}!`);
 };
