fb4082f0c9c0914ad61eb3653d5d473235e25bd6
eunsoooo <shines1427@gmail.com>
2022-06-06 23:34:32 +0900

    Refactor: router로 분리해서 작성

--- src/server.js
+++ src/server.js
@@ -1,24 +1,20 @@
 import express from "express";
 import morgan from "morgan";
 
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+
 // TODO: env 파일로 빼기
 const PORT = 3000;
-const NODE_ENV = "dev";
 
 const app = express();
-const loggerMiddleware = NODE_ENV === "dev" ? morgan("dev") : morgan("common");
-
-const handleHome = (req, res) => {
-  return res.send("home");
-};
-
-const handleLogin = (req, res) => {
-  return res.send("login");
-};
+const loggerMiddleware = morgan("common");
 
 app.use(loggerMiddleware);
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 const handleListening = () => {
   console.log(`✅ Server listening on port ${PORT}!`);
